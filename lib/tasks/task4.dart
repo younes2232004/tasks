51c95ab5139d72981ac5223a12b412d7174868f0
younis <yalmayth88@gmail.com>
2025-02-04 14:18:16 +0300

    task.4 4/2/2025 2:18

--- lib/tasks/task4.dart
+++ lib/tasks/task4.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class SliverAppBarExample extends StatelessWidget {
-  const SliverAppBarExample({super.key});
+class Task4 extends StatelessWidget {
+  const Task4({super.key});
 
   @override
   Widget build(BuildContext context) {
