f5ae6ce15f6cac804eadcc148e36ff49e45efe48
younis <yalmayth88@gmail.com>
2025-02-04 14:16:19 +0300

    task.3 4/2/2025 2:16

--- lib/tasks/task3.dart
+++ lib/tasks/task3.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ListViewExample extends StatelessWidget {
-  ListViewExample({super.key});
+class Task3 extends StatelessWidget {
+  Task3({super.key});
   final products = List.generate(100, (index) => 'Product $index');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ListView Example'),
+        title: const Text('Task3'),
       ),
       body: ListView.builder(
         itemCount: products.length,
