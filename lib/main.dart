05f8b5f7169d03202953c72e75e183a1aaf946ac
younis <yalmayth88@gmail.com>
2025-02-06 13:44:34 +0300

    task.6 6/2/2025 1:44

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 // import 'tasks/task1.dart';
 // import 'tasks/task2.dart';
-import 'tasks/task5.dart';
+import 'tasks/task6.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       // this is task selector
       //home: const Task1(),
       //home: const Task2(),
-      home: Task5(),
+      home: Task6(),
     );
   }
 }
