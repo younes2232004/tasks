4989c27102eecda4d1dc1220bb6d7c214a8e4b29
younis <yalmayth88@gmail.com>
2025-02-18 14:43:16 +0300

    task16 28/2/2025 2:43

--- lib/components/task16.dart
+++ lib/components/task16.dart
@@ -1,1 +1,66 @@
+// task 16 reusable compo
+import 'package:flutter/material.dart';
 
+class Task16 extends StatelessWidget {
+  const Task16({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Reusable components"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            // 1
+            Row(
+              children: [
+                CustomButton(
+                  text: 'Button1',
+                  onPressed: () {
+                    print("Button1 Pressed");
+                  },
+                ),
+                CustomButton(
+                  text: 'Button2',
+                  onPressed: () {
+                    print("Button2 Pressed");
+                  },
+                ),
+              ],
+            ),
+            // 2
+            ElevatedButton(
+              onPressed: () {
+                print("Button2 Pressed");
+              },
+              child: Text("Button2"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))),
+      onPressed: onPressed,
+      child: Text(text),
+    );
+  }
+}
